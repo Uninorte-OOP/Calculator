8d2b95f83727e8414559133ffa09b2c74afa9a8f
jmartch <160760617+jmartch@users.noreply.github.com>
2024-05-25 11:41:04 -0500

    Carpertas y Clases

--- src/calculator/view/CalculatorFrame.java
+++ src/calculator/view/CalculatorFrame.java
@@ -2,8 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package calculator;
+package calculator.view;
 
+import calculator.Main.Calculator;
+import calculator.History;
+import calculator.model.Operation;
 import java.util.ArrayList;
 import java.util.Collections;
 import javax.swing.DefaultListModel;
